9020e04fa51ea33a175da9dbda9982bb9ef1d710
Torben Toepper <tt@htmlcast.de>
2013-08-03 13:08:37 +0200

    Adds new field in navigation

--- db/migrations/2_articles.js
+++ db/migrations/2_articles.js
@@ -3,6 +3,10 @@ module.exports = {
   tableName: 'articles',
   fields: [
     {
+      name: "navigation_id",
+      type: "integer"
+    },
+    {
       name: "author_id",
       type: "integer"
     },
