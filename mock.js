a3650c2632928a78d6c13fa444205116ef5ab2d1
Torben Toepper <tt@htmlcast.de>
2013-08-03 13:08:43 +0200

    Mocking stuff

--- mock.js
+++ mock.js
@@ -1,7 +1,29 @@
-Article = require("./app/models/article");
+var Article = require('./app/models/article');
+var Navigation = require('./app/models/navigation');
 
+
+/*
 Article.fields(function() {
   article = new Article();
   article.id = 6
   console.log(article.id);
+});
+*/
+
+Article.fields(function() {
+  var index = 1;
+  Article.all(function(err, articles) {
+    articles.forEach(function(article) {
+      console.log("navigationID: "+ article.navigation_id, index)
+      article.navigation_id = index;
+      
+      console.log("changed: ", article.changedAttributes)
+      console.log(article.navigation_id)
+
+      article.save(function(err) {
+        console.log(arguments)
+      });
+      index++;
+    });
+  });
 });
